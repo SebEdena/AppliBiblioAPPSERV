6332a0917d2312ca8033fa9dc4aada643037afc7
Mathieu Valentin <mathieu.valentin1@orange.fr>
2016-12-17 17:27:59 +0100

    erreur ?

--- src/documents/Livres.java
+++ src/documents/Livres.java
@@ -1,5 +1,33 @@
 package documents;
 
-public class Livres {
+import bibliotheque.Abonne;
+import bibliotheque.Document;
+import bibliotheque.PasLibreException;
+
+public class Livres implements Document{
+
+	@Override
+	public int numero() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void reserver(Abonne ab) throws PasLibreException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void emprunter(Abonne ab) throws PasLibreException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void retour() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
